d34ecc518b556942ef348929db2ec1a55c84d2ff
Joe Borg <cyborg101010@gmail.com>
2014-06-28 14:27:48 +0000

    Relese 0.2 for URL fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info >= (3,0):
 
 setup (
     name = 'mailer3',
-    version = "0.1",
+    version = "0.2",
     description = "A copy of https://pypi.python.org/pypi/mailer with 2to3 run over all the .py files.",
     author = "Joe Borg",
     url = "https://github.com/joedborg/mailer3",
